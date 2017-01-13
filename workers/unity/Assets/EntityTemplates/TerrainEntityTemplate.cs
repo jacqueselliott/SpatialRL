59a24f9218e6f8600ac46983b0ac8463845d1701
Jacques Elliott <jge29@cam.ac.uk>
2017-01-13 16:42:35 +0000

    first commit

---
+++ workers/unity/Assets/EntityTemplates/TerrainEntityTemplate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Improbable.Worker;
+using Terrain = Improbable.Terrain.Terrain;
+using TerrainData = Improbable.Terrain.TerrainData;
+using Improbable.General;
+using Improbable.Math;
+using Improbable.Unity.Core.Acls;
+
+namespace Assets.EntityTemplates
+{
+    public class TerrainEntityTemplate : MonoBehaviour
+    {
+
+        public static SnapshotEntity GenerateTerrainSnapshotEntityTemplate()
+        {
+            var terrain = new SnapshotEntity { Prefab = "Terrain" };
+            terrain.Add(new Terrain.Data(new TerrainData()));
+            terrain.Add(new WorldTransform.Data(new WorldTransformData(new Coordinates(0, 0, 0), 0)));
+
+            var acl = Acl.Build()
+                .SetReadAccess(CommonPredicates.PhysicsOrVisual)
+                .SetWriteAccess<WorldTransform>(CommonPredicates.PhysicsOnly);
+            terrain.SetAcl(acl);
+
+            return terrain;
+        }
+    }
+}
